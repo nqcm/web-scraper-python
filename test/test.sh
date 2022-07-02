447426cbca0c48eb353e67d1b563469a2dcc47f5
tadhg <tadhg.davey@credera.co.uk>
2022-07-02 16:06:32 +0100

    debugging

--- test/test.sh
+++ test/test.sh
@@ -2,11 +2,11 @@
 
 set -euo pipefail
 
-#python3 ../src/scraper/scrape.py https://webscraper.io/test-sites/e-commerce/allinone
+python3 ../src/scraper/scrape.py https://webscraper.io/test-sites/e-commerce/allinone
 curl -o trolley.png https://webscraper.io/images/test-sites/e-commerce/items/cart2.png
 
 read -ra realsha <<< $(sha256sum trolley.png)
-read -ra scrapesha <<< $(sha256sum twicon.png)
+read -ra scrapesha <<< $(sha256sum cart2.png)
 
 if [ "$realsha" == "$scrapesha" ]; then exit 0
 else exit 1
